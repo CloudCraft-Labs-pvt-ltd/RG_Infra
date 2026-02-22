5135828a2d268aba6744a5812721d6d5ef7984dd
Vaibhav Khupse <vaibhavkhupse@gmail.com>
2026-02-22 21:00:32 +0530

    multienv

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stgformstate231023"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
